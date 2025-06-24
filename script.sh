62750929fbfd9723e197859835eee3e897f94303
AbiCaleb <112403958+AbiCaleb@users.noreply.github.com>
2025-06-24 21:26:22 +0530

    monkey

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ echo "Listing Files"
 ls -l
 
 echo "generating cosway"
-cowsay -f dragon "Naa Nerupppu kakum dragon daa... grrwwrr..." >> dragon.txt
+cowsay -f monkey "Naa Nerupppu kakum dragon daa... grrwwrr..." >> monkey.txt
 
-echo "View the dragon"
-cat dragon.txt
+echo "View the monkey"
+cat monkey.txt
 
 echo "file exist test"
-grep -i "dragon" dragon.txt
+grep -i "monkey" monkey.txt
 
-echo "Listing Files after dragon file added"
+echo "Listing Files after monkey file added"
 ls -l
